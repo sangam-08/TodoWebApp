48f753e4682599b038de07da31ae095a9393b8f9
sangam-08 <sangam.kumar@impressico.com>
2021-07-26 12:22:13 +0530

    code formate

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const express = require('express')
-const students = require('./data')
+const express = require("express");
+const students = require("./data");
 const app = express();
-app.use(express.json())
-app.listen(8080,()=>{
-    console.log("running on port 8080");
-})
+app.use(express.json());
+app.listen(8080, () => {
+  console.log("running on port 8080");
+});
 
-app.get('/',(req,res)=>{
-    res.json({message:'Api is working fine'})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "Api is working fine" });
+});
 
-app.get('/api/students',(req,res)=>{
-    res.json(students)
-})
+app.get("/api/students", (req, res) => {
+  res.json(students);
+});
 
-app.post('/api/students',(req,res)=>{
-    if(!req.body.email) {
-        res.status(400);
-        return res.json({message:'email is required'})
-    }
-    const user = {
-    id: students.length+1,
+app.post("/api/students", (req, res) => {
+  if (!req.body.email) {
+    res.status(400);
+    return res.json({ message: "email is required" });
+  }
+  const user = {
+    id: students.length + 1,
     first_name: req.body.first_name,
-    last_name: req.body.last_name ,
-    email: req.body.email
-    }
-    students.push(user)
-    console.log(req.body);
-    res.json(user)
-})
+    last_name: req.body.last_name,
+    email: req.body.email,
+  };
+  students.push(user);
+  console.log(req.body);
+  res.json(user);
+});
