40596eebaa1aa90e7c6d76c39be2e25d89e3797f
manish.tyagi <manish.tyagi@impressico.com>
2021-07-26 10:12:32 +0530

    get/post is made

---
+++ data.js
@@ -0,0 +1,53 @@
+const students = [{
+    "id": 1,
+    "first_name": "Carline",
+    "last_name": "Starkie",
+    "email": "cstarkie0@flickr.com"
+  }, {
+    "id": 2,
+    "first_name": "Tammi",
+    "last_name": "Fessby",
+    "email": "tfessby1@arizona.edu"
+  }, {
+    "id": 3,
+    "first_name": "Mar",
+    "last_name": "Benson",
+    "email": "mbenson2@so-net.ne.jp"
+  }, {
+    "id": 4,
+    "first_name": "Damon",
+    "last_name": "Marfe",
+    "email": "dmarfe3@yahoo.com"
+  }, {
+    "id": 5,
+    "first_name": "Nathalia",
+    "last_name": "Normanton",
+    "email": "nnormanton4@hp.com"
+  }, {
+    "id": 6,
+    "first_name": "Virginie",
+    "last_name": "Kings",
+    "email": "vkings5@eventbrite.com"
+  }, {
+    "id": 7,
+    "first_name": "Nonie",
+    "last_name": "Sinclar",
+    "email": "nsinclar6@fda.gov"
+  }, {
+    "id": 8,
+    "first_name": "Ina",
+    "last_name": "Bruce",
+    "email": "ibruce7@pcworld.com"
+  }, {
+    "id": 9,
+    "first_name": "Jacques",
+    "last_name": "Abbs",
+    "email": "jabbs8@nbcnews.com"
+  }, {
+    "id": 10,
+    "first_name": "Ferrel",
+    "last_name": "Dalziell",
+    "email": "fdalziell9@mtv.com"
+  }]
+
+  module.exports = students
